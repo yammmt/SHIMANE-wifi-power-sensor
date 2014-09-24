c5bd10754ce9b36969f23c79f510bfe25f72fbcd
Super-ito <cu.cs.y20@gmail.com>
2014-09-24 23:20:28 +0900

    changed by button, but We can't use Wi-Fi function with button

--- js/main.js
+++ js/main.js
@@ -8,34 +8,15 @@ Img[2].src = "img/ehime.png";
 Img[3] = new Image();
 Img[3].src = "img/tokushima.png";
 
-var NowShimane = 1;
-
-apealShimane = function() {
-    console.log("apeal");
-    if(nowShimane) {
-        alert("YES!");
-    }
-    else {
-        alert("no...");
-    }
-}
-
 changeShimane = function() {
     console.log("changeShimane");
     var nextImg = Math.round(Math.random()*3);
-    if(nextImg == 0) {
-        nowShimane = 1;
-    }
-    else {
-        nowShimane = 0;
-    }
-    document.getElementById("pref").src = Img[nextImg].src;
-}
+    document.getElementById("shimaneButton").src = Img[nextImg].src;
+};
 
 getWifiPower = function() {
     console.log("getWifiPower");
 
-/*
     // 4debug
     var netNum = Math.round(Math.random()*15);
     var aveNum = Math.round(Math.random()*100)+1;
@@ -44,46 +25,45 @@ getWifiPower = function() {
     document.getElementById("num").innerHTML = "検出数 : " + netNum;
     document.getElementById("ave").innerHTML = "平均強度 : " + aveNum;
     document.getElementById("pow").innerHTML = "総合電波力 : " + netPow;
-
     changeShimane();
-*/
 
+// 以下, manifest.webapp に type:"certified" を追加しなければ動かず
+// 追加するとボタン無効に
+/*
     var wifi = navigator.mozWifiManager;
-    var request = wifi.getNetworks();
 
-    request.onsuccess = function () {
-	var networks = this.result;
-	var numElm = document.getElementById("num");
-	var aveElm = document.getElementById("ave");
-	var powElm = document.getElementById("pow");
+    if(wifi) {
+        var request = wifi.getNetworks();
+        request.onsuccess = function () {
+	    var networks = this.result;
+	    var numElm = document.getElementById(""num");
+	    var aveElm = document.getElementById("ave");
+	    var powElm = document.getElementById("pow");
 
-	var netNum = 0;
-	var netPow = 0;
+	    var netNum = 0;
+	    var netPow = 0;
 
-        networks.forEach(function (network) {
-	    netNum++;
-	    netPow += network.relSignalStrength;
-        });
-	aveNum = Math.round(netPow/netNum);
+            networks.forEach(function (network) {
+	        netNum++;
+	        netPow += network.relSignalStrength;
+            });
+	    aveNum = Math.round(netPow/netNum);
 
-	numElm.innerHTML = "検出数 : " + netNum;
-        aveElm.innerHTML = "平均強度 : " + aveNum;
-	powElm.innerHTML = "総電波力 : " + netPow;
+	    numElm.innerHTML = "検出数 : " + netNum;
+            aveElm.innerHTML = "平均強度 : " + aveNum;
+	    powElm.innerHTML = "総電波力 : " + netPow;
 
-        changeShimane(2*netPow);
-    }
+            changeShimane();
+        }
 
-    request.onerror = function (err) {
+        request.onerror = function (err) {
 	    var infoElm = document.getElementById("info");
 	    infoElm.innerHTML += 'エラーが発生しました。再読み込みしてください。';
-    };
-
-}
-
-testFunc = function() {
-    getWifiPower();
-    setTimeout("testFunc()", 5000);
+        }
+    }
+    else {
+        window.alert("NO Wi-Fi err");
+    }
+*/
 };
-
-document.addEventListener("DOMContentLoaded", testFunc);
 
