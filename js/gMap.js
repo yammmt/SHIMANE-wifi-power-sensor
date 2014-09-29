98c917c971f4d4c7199571f0a0923370b38f34c4
Super-ito <cu.cs.y20@gmail.com>
2014-09-30 00:48:54 +0900

    confirmed by Flame

--- js/gMap.js
+++ js/gMap.js
@@ -6,33 +6,8 @@ doAction = function() {
     navigator.geolocation.getCurrentPosition(getPosition, error, opt)
 };
 
-test = function(lat,lng) {
-/*
-    var map = new google.maps.Map(
-        document.getElementById("gmap"), {
-            zoom : 7,
-            center : new google.maps.LatLng(lat, lng),
-	    mapTypeId : google.maps.MapTypeId.ROADMAP
-        }
-    );
-*/
+generateMap = function(lat,lng) {
     var latlng = new google.maps.LatLng(lat, lng);
-    var mapOptions = {
-        position: latlng,
-        pov: {
-            heading: 0,
-            pitch: 5
-        }
-    };
-/*
-    var myMap = new google.maps.Map(
-        document.getElementById("gmap"), {
-            zoom:20,
-            center: latlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-    );
-*/
     var myMap = new google.maps.StreetViewPanorama(
         document.getElementById("gmap"), {
             position: latlng,
@@ -47,24 +22,11 @@ test = function(lat,lng) {
 getPosition = function(p) {
     var lat = p.coords.latitude;
     var lng = p.coords.longitude;
-    //generateMap(lat, lng);
-    test(lat,lng);
+    generateMap(lat, lng);
 };
 
 error = function(err) {
     alert("ERROR("+err.code+")/n"+err.message);
-};
-
-generateMap = function(lat, lng) {
-    var latlng = new google.maps.LatLng(lat, lng);
-    var mapOptions = {
-        position: latlng,
-        pov: {
-            heading: 0, 
-            pitch: 5
-        }
-    };
-    var myMap = new google.maps.StreetViewPanorama(document.getElementById('gmap'), mapOptions);
 };
 
 back2Shimane = function() {
