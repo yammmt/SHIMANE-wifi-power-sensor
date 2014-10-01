f9ff86080b82fce336d06ebff3b68d78eaa469b3
Super-ito <cu.cs.y20@gmail.com>
2014-10-01 12:42:35 +0900

    auto map change by latlng

--- js/gMap.js
+++ js/gMap.js
@@ -1,41 +1,44 @@
+var LATLNG;
+
 doAction = function() {
     var opt = {
         enableHighAccuracy: true,
         timeout: 10000
     };
-    navigator.geolocation.getCurrentPosition(getPosition, error, opt)
+    navigator.geolocation.getCurrentPosition(initPosition, error, opt)
 };
 
-generateMap = function(lat,lng) {
-    var latlng = new google.maps.LatLng(lat, lng);
-    console.log(latlng);
-
-    var mapOptions = {
-        position: latlng,
-        pov: {
-            heading: 0,
-            pitch: 5
-        }
-    };
-    var myMap = new google.maps.StreetViewPanorama(document.getElementById('gmap'), mapOptions);
-
-/*
-    var mapOptions = {
-        zoom: 20,
-        center: latlng,
-        scaleControl: false,
-        mapTypeControl : false,
-        disableDoubleClickZoom: true,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    };
-    var myMap = new google.maps.Map(document.getElementById('gmap'), mapOptions);
-*/
-
+generateMap = function(data, status) {
+    if(status != google.maps.StreetViewStatus.OK) {
+        console.log("NG");
+        var mapOptions = {
+            zoom: 20,
+            center: LATLNG,
+            scaleControl: false,
+            mapTypeControl: false,
+            panControl: false,
+            zoomControl: false,
+            disableDoubleClickZoom: true,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+        };
+        var myMap = new google.maps.Map(document.getElementById('gmap'), mapOptions);
+    }
+    else {
+        console.log("OK");
+        var mapOptions = {
+            position: data.location.latLng,
+            pov: {
+                heading: 0,
+                pitch: 5
+            }
+        };
+        var myMap = new google.maps.StreetViewPanorama(document.getElementById('gmap'), mapOptions);
+    }
     console.log('wifiNum:' + getCookie('wifiNum'));
     drawLine(Number(getCookie('wifiNum')));
 };
 
-// クッキーの値を取得 getCookie(クッキー名); //
+// クッキーの値を取得 getCookie(クッキー名);  (引用) //
 function getCookie(c_name){
     var st = "";
     var ed = "";
@@ -51,10 +54,15 @@ function getCookie(c_name){
     return "";
 }
 
-getPosition = function(p) {
+initPosition = function(p) {
     var lat = p.coords.latitude;
     var lng = p.coords.longitude;
-    generateMap(lat, lng);
+
+    LATLNG = new google.maps.LatLng(lat, lng);
+    console.log(LATLNG);
+
+    var sv = new google.maps.StreetViewService();
+    sv.getPanoramaByLocation(LATLNG, 50, generateMap); 
 };
 
 error = function(err) {
